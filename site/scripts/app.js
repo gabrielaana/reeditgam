f8992f6c82fe83579c251e9da0922c7773a19815
gabrielaana <gabriela.aldana8@gmail.com>
2015-05-07 13:02:52 -0500

    09. Sea agrega un servicio del tipo Factory para alamcenar los posts

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,40 +1,63 @@
 console.log ("#Gaby: Cargo app.js");
 var modulo1=
 	angular.module("reeditgam",[]);
+//CREANDO UN SERVICIO del tipo factory
+modulo1.factory('posts',[function(){
+// cuerpo del factory llamado post
+var o= {
+posts : [
+   {
+     title: "post 1", upvotes: 15,
+     comments: [
+        {author: "Karina", body:"Esto esta de pelos.",
+        upvotes:3},
+        {author: "Gamaliel", body:"Esto es basura.",
+        upvotes:0}]
+    },
+    {
+     title: "post 2", upvotes: 4,
+     comments: [
+        {author: "Coco", body:"Esto es asombroso.",
+        upvotes:5},
+        {author: "Cristian", body:"Esto esta aburrido.",
+        upvotes:1}]
+    }
+         ]
+     };
+     //retornando objeto de datos persistentes
+     return o;
+}]);
+	//CREANDO CONTROLADOR
+	// dependcy injection
 modulo1.controller("mainCtrl",[
-	'$scope',
-	function($scope){
+	'$scope','posts',  // Inyectando factory post
+	function($scope, posts){
 		$scope.test = "Hola Angular";
-		//Modelo
-		$scope.posts = 
-		[{title: "Post 1", upvotes: 5},
-		 {title: "Post 2", upvotes: 15},
-		 {title: "Post 3", upvotes: 7},
-		 {title: "Post 4", upvotes: 9},
-		 {title: "Post 5", upvotes: 3},
-		 {title: "Post 6", upvotes: 5}];
+		//Modelo al cual se le asigna 
+		//el resultado del factory
+		$scope.posts = posts.posts;
+	  
+		//Metodo del controlador
 
-		 //Metodo del controlador 
-		 $scope.addPost = function(){
-		if(!$scope.title || $scope.title === "")
-		{
-			alert("No se permite postear titulos vacios")
-			return;
-		}
-              $scope.posts.push(
-              	{
-              		title:$scope.title,
-              		link:$scope.link,
-                    upvotes: 0
-                });
-              // Two-way data binding
-              $scope.title = "";
-              $scope.link = "";
-		 };
-		  //Metodo que incrementa el voto de un post en una unidad
-
-		 $scope.incrementUpvotes = function(post){
-		 	post.upvotes += 1;
-		 };
-
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === "")
+			{
+              alert("NO se permite postear titulos vacios")
+              return;
+			}
+		$scope.posts.push(
+			{
+				title:$scope.title,
+				link:$scope.link,
+			 upvotes: 0
+			});
+           //Two-way data bindingit
+           $scope.title = "";
+            $scope.link = "";
+	    };	
+	    //metodo que incrementa
+	    // el voto de un post en una unidad
+	    $scope.incrementUpvotes = function(post){
+             post.upvotes += 1;
+	    }
 	}]);
